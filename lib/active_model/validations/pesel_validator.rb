e444d89ee11ba947e758dc2fe7215dccd8dfe2a6
Szymon Rut <rut.szymon@gmail.com>
2022-10-11 09:39:44 +0200

    Make validate_each method compatible with Rails 6.1

--- lib/active_model/validations/pesel_validator.rb
+++ lib/active_model/validations/pesel_validator.rb
@@ -2,9 +2,7 @@ module ActiveModel
   module Validations
     class PeselValidator < ActiveModel::EachValidator
       def validate_each(record, attr_name, value)
-        unless value.blank?
-          record.errors.add(attr_name, :invalid, options) unless Activepesel::Pesel.new(value).valid?
-        end
+        record.errors.add(attr_name, :invalid, **options) if !value.blank? && !Activepesel::Pesel.new(value).valid?
       end
     end
   end
